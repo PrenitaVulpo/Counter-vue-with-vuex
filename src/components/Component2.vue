c02c5bf38c3b35a3ea054e7279e9bf3c4911c60e
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-08-17 10:19:40 -0300

    bugfix

--- src/components/Component2.vue
+++ src/components/Component2.vue
@@ -5,7 +5,7 @@ import { defineComponent } from 'vue';
 import Component3 from './Component3.vue';
 
 export default defineComponent({
-	name: 'Component3',
+	name: 'Component2',
 	components: { Component3 },
 });
 </script>
